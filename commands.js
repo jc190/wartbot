cc7f2179e61729d8daa68491dffc936609026d37
James Calhoun <15990874+jc190@users.noreply.github.com>
2020-06-16 21:44:33 -0400

    fix gamedeals limit

--- commands.js
+++ commands.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 const commands = {};
 
 commands.gamedeals = (msg) => {
-  fetch('https://old.reddit.com/r/GameDeals/.json?limit=4')
+  fetch('https://old.reddit.com/r/GameDeals/.json?limit=5')
     .then((data) => data.json())
     .then((json) => {
       return json.data.children.map((post) => {
@@ -12,7 +12,7 @@ commands.gamedeals = (msg) => {
       });
     })
     .then((data) => {
-      msg.author.send('Here are the top 5 deals going on today. Source (<https://reddit.com/r/gamedeals>)');
+      msg.author.send('Here are 5 trending deals going on today. Source (<https://reddit.com/r/gamedeals>)');
       msg.author.send(data.join("\n\n"))
         .catch(() => msg.author.send('Something went terribly wrong :('));
     })
